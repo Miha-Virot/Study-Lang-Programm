a1ffa745ee3e0b33865a3ad08e3eefa2a5b3eeb0
Михаил Вирот <61559471+Miha-Virot@users.noreply.github.com>
2021-03-27 20:12:06 +0300

    1

---
+++ Java/Математические операции/src/main.java
@@ -0,0 +1,19 @@
+
+public class main {
+// res++ Ч прибавление единицы
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float num_1=23.67f, num_2=43.4f, res;
+		res = num_1+num_2; //res+=45f
+		System.out.println(res);
+		res = num_1-num_2;
+		System.out.println(res);
+		res = num_1*num_2;
+		System.out.println(res);
+		res = num_1/num_2;
+		System.out.println(res);
+		res = num_1%num_2;
+		System.out.println(res);
+	}
+
+}
---
+++ Java/Усл.оператор switch/src/main.java
@@ -0,0 +1,26 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=23;
+		switch (num) {
+			case 12:
+				System.out.print("Num is 12");
+				break;
+			case 545:
+				System.out.print("Num is 545");
+				break;
+			case 23:
+				System.out.println("Num is 23");
+				if (num==23) {
+					System.out.println("All is OK");
+				}
+				break;
+
+			default:
+				System.out.print("Num is unknown");
+			}
+	}
+
+}
