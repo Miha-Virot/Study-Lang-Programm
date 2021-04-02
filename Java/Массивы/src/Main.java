67d530fed02303b9694944dbba853d59397108f4
Михаил Вирот <61559471+Miha-Virot@users.noreply.github.com>
2021-04-02 21:12:25 +0300

    1

---
+++ Java/Массивы/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+	public static void main (String[] args) {
+		/*int [] arr = new int[3];
+		arr[0]=23;
+		arr[2]=657;
+		System.out.print(arr[2]); */
+		int [] arr = new int[] {55, 489,325, 678};
+		for (int i=1; i<4;i++) {
+			System.out.println("Element " + i + " is " + arr[i]);
+		}
+	}
+}
