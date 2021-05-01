79a2c9b48cd147228fa4b4fc8eb0f1227a8b460a
Михаил Бенке <mishabenke@inbox.ru>
2021-05-01 23:28:31 +0300

    1

---
+++ C++/Упражнения с informatics/Глава 1/Работа с текстом/1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+/*
+int main(){
+    ifstream in;  // Поток in будем использовать для чтения
+    ofstream out; // Поток out будем использовать для записи
+    in.open("input.txt");
+    out.open("output.txt");
+    out << x; // вывод значения
+    in >> x; // ввод значения
+    in.close(); // закрыть ранее открытый файл в потоке
+    out.close(); // 
+}
+*/
+
+int maint()
+{
+    ifstream in;
+    ofstream out;
+    in.open("input.txt");
+    out.open("output.txt");
+    int a, b = 0;
+    string S;
+}
