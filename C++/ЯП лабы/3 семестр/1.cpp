0f7bbd8ed9d28018a2c9d3f498e3ff37ef682653
Михаил Вирот <61559471+Miha-Virot@users.noreply.github.com>
2021-12-13 18:19:25 +0300

    1

---
+++ C++/ЯП лабы/3 семестр/1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+
+class Caesar()
+{
+private:
+    int i, key;
+    string message, crypt;
+    char list[28] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+public:
+    Caesar()
+    {
+        this->key = 0;
+        this->message = ""; // указатель "this" указывает что именно менять надо приватную переменную, а не ту что в методе используется
+        /*
+            key = random();
+            message = random();
+            */
+        // тут буду испльзовать рандомайзер, спец функцию
+    }
+    void printData()
+    {
+        cout << this->key << this->message;
+    }
+    ~Caecar()
+    {
+        delete this->i, this->key, this->message, this->crypt;
+    }
+}
+
+int main()
+{
+    Caesar one();
+    one.printData;
+    delete one; // удалить объект, требуется уточнить про область видимости и
+    return 0;
+}
